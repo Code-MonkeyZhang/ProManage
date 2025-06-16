074f5f77f59e5c6e4d55650e144f63d6d594aa73
Jonathan Zhang <yufengzhang483@gmail.com>
2025-06-16 13:50:42 -0400

    setup pages and nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+//styles
+import "./App.css";
+
+// pages and components
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Project from "./pages/project/Project";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Dashboard></Dashboard>
+            </Route>
+            <Route path="/create">
+              <Create></Create>
+            </Route>
+            <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="/signup">
+              <Signup></Signup>
+            </Route>
+            <Route path="/project/:id">
+              <Project></Project>
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
