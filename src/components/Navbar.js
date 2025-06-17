c81d7d56cee0552dd359720ebb3840597e838b32
Jonathan Zhang <yufengzhang483@gmail.com>
2025-06-17 02:29:51 -0400

    enable login

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import { useLogin } from "../hooks/useLogin";
 
 // Styles & Images
 import "./Navbar.css";
 import Temple from "../assets/temple.svg";
 
-import React from "react";
-
 export default function Navbar() {
+  const { logout, isPending, error } = useLogout();
+
   return (
     <div className="navbar">
       <ul>
@@ -21,7 +23,16 @@ export default function Navbar() {
           <Link to="/signup">Signup</Link>
         </li>
         <li>
-          <button className="btn">Logout</button>
+          {!isPending && (
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          )}
+          {isPending && (
+            <button className="btn" disabled>
+              Logging out...
+            </button>
+          )}
         </li>
       </ul>
     </div>
