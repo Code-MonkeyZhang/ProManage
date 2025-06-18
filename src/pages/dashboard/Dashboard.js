5b1e812830222c3a4b68e4b14499f8ca2c48e6fc
Jonathan Zhang <yufengzhang483@gmail.com>
2025-06-18 15:12:43 -0400

    add project list and add them to Dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,5 +1,17 @@
-import React from "react";
+import { useCollection } from "../../hooks/useCollection";
+import ProjectList from "../../components/ProjectList";
+
+// styles
+import "./Dashboard.css";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("project");
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard </h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 }
