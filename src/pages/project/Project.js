55b623c3053ad85954f08278d214313c34b9f47a
Jonathan Zhang <yufengzhang483@gmail.com>
2025-06-18 20:29:04 -0400

    enable profile detail page

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,5 +1,24 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
+
+//styles
+import "./Project.css";
 
 export default function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { error, document } = useDocument("project", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">Loading....</div>;
+  }
+
+  return (
+    <div className="project-details">
+      <h1>{document.name}</h1>
+    </div>
+  );
 }
